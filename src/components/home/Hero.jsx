5e0c6d11663f8bfe5cc7eba550eede3cc40634d7
Prosper Success <osiletusuccess@gmail.com>
2023-03-23 02:15:24 +0100

    improved quiz function

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -24,13 +24,13 @@ const handleAnswer = isCorrect =>{
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content text-center">
     <div className="max-w-md ">
-      {questions.map((qst, id)=>(
-        <div>
-          <h1 className="flex justify-start text-3xl">{qst.id}</h1>
-          <h2 key={id} className='text-black text-bold text-2xl py-3'>{qst.qstText}</h2>
-          <p className="text-black" >{qst.answers.map(ans=>(
+      <h1 className="flex justify-start text-3xl"></h1>
+      <h2 className='text-black text-bold text-2xl py-3'>{questions[currentQuestion].qstText}</h2>
+      {questions[currentQuestion].answers.map(ans =>(
+        <div key={ans.id}>
+        
             <span className="flex flex-col justify-between my-6" > <button onClick={()=> handleAnswer(ans.isCorrect)} className="py-3 px-4 bg-yellow-500 rounded text-center" >{ans.answer}</button> </span>
-          ))}</p>
+        
         </div>
         
 ))}
